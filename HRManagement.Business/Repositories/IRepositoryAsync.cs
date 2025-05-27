3224b5b080d217460c0ce68f8140fe2bcc4b0069
Datnekk <quangdat@gmail.com>
2025-05-27 18:22:21 +0700

    feat/JWT

---
+++ HRManagement.Business/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,10 @@
+namespace HRManagement.Business.Repositories;
+
+public interface IRepositoryAsync<T> where T : class
+{
+    Task<IEnumerable<T>> GetAsync();
+    Task<T> GetByIdAsync(int id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
---
+++ HRManagement.Business/Repositories/ITokenRepository.cs
@@ -0,0 +1,13 @@
+using HRManagement.Business.Dtos.Auth;
+using HRManagement.Data.Entity;
+using Microsoft.AspNetCore.Http;
+
+namespace HRManagement.Business.Repositories;
+
+public interface ITokenRepository
+{
+    Task<TokenDTO> CreateJWTTokenAsync(User user, bool populateExp);
+    Task<TokenDTO> RefreshJWTTokenAsync(TokenDTO tokenDTO);
+    void SetTokenCookie(TokenDTO tokenDTO, HttpContext context);
+    void DeleteTokenCookie(HttpContext context);    
+}

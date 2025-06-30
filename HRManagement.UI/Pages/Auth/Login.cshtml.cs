725be3e371f3b84ba61900c950935b5578b939eb
DongVTHE176699 <Dongvthe176699@gmail.com>
2025-06-30 21:59:41 +0700

    Add Login, resgister, Layout Employye

---
+++ HRManagement.UI/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRManagement.UI.Pages.Auth
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
---
+++ HRManagement.UI/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRManagement.UI.Pages.Auth
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 

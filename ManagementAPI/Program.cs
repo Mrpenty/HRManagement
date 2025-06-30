4456c79c79b495c044b29783534f286d2f033258
KhanhDo <dovietkhanh02082003@gmail.com>
2025-06-30 22:20:28 +0700

    a

--- ManagementAPI/Program.cs
+++ ManagementAPI/Program.cs
@@ -1,16 +1,13 @@
-using HRManagement.Data.Data;
+﻿using HRManagement.Data.Data;
 using ManagementAPI.Extensions;
 using Microsoft.EntityFrameworkCore;
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<HRManagementDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn")));
 
-// Configure services
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCorsServices(builder.Configuration, builder.Environment); // ✅ Đúng vị trí
 builder.Services.AddSwaggerServices();
 builder.Services.AddControllersServices();
 builder.Services.AddDatabaseServices(builder.Configuration);
@@ -18,21 +15,22 @@ builder.Services.AddIdentityServices();
 builder.Services.AddAuthenticationServices(builder.Configuration);
 builder.Services.AddDependencyInjectionServices();
 builder.Services.AddAutoMapperServices();
-builder.Services.AddCorsServices(builder.Configuration, builder.Environment);
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseCorsPolicy(builder.Environment); // ✅ Gọi trước Authentication/Authorization
+
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapRazorPages();
 app.MapControllers();
-app.UseCorsPolicy(builder.Environment);
 app.UseSwaggerServices(builder.Environment);
+
 app.Run();
